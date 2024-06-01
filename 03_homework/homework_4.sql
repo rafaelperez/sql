bf519294a67f6cb803a636572f085e411f89bc24
Rafael Silva <rafael@rafael.ai>
2024-06-01 18:03:00 -0400

    homework 04

--- 03_homework/homework_4.sql
+++ 03_homework/homework_4.sql
@@ -17,8 +17,9 @@ The `||` values concatenate the columns into strings.
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
 
-
-
+SELECT 
+product_name || ', ' || COALESCE(product_size, '') || ' (' || COALESCE(product_qty_type, 'unit') || ')'
+FROM product
 
 --Windowed Functions
 /* 1. Write a query that selects from the customer_purchases table and numbers each customer’s  
@@ -30,17 +31,62 @@ each new market date for each customer, or select only the unique market dates p
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 
+SELECT DISTINCT
+    customer_id,
+    market_date,
+    DENSE_RANK() OVER (
+        PARTITION BY customer_id 
+        ORDER BY market_date
+        ) as visit_id
+FROM
+    customer_purchases;
 
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
 
+SELECT *
+FROM (
+    SELECT DISTINCT
+        customer_id,
+        market_date,
+        DENSE_RANK() OVER (
+            PARTITION by customer_id
+            ORDER BY market_date DESC
+            ) AS visit_id
+    FROM customer_purchases
+) AS visits
+WHERE visit_id = 1;
+
+-- alternative
+
+WITH visits AS (
+    SELECT DISTINCT
+        customer_id,
+        market_date,
+        DENSE_RANK() OVER (
+            PARTITION by customer_id
+            ORDER BY market_date DESC
+            ) AS visit_id
+    FROM customer_purchases
+)
+SELECT *
+FROM visits
+WHERE visit_id = 1;
 
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
 
-
-
+SELECT 
+    customer_id,
+    product_id,
+    quantity,
+    COUNT(*) OVER (
+        PARTITION BY customer_id,
+        product_id
+        ) as num_purchases
+FROM 
+    customer_purchases;
 
 -- String manipulations
 /* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
@@ -54,11 +100,25 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
-
+SELECT 
+    product_name,
+    TRIM(
+        SUBSTR(product_name, INSTR(product_name, '-') + 1)
+        ) AS description
+FROM product
+WHERE INSTR(product_name, '-') > 0;
 
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
 
-
+SELECT 
+    product_name,
+    product_size,
+    TRIM(
+        SUBSTR(product_name, INSTR(product_name, '-') + 1)
+        ) AS description
+FROM product
+WHERE INSTR(product_name, '-') > 0
+AND product_size REGEXP '[0-9]';
 
 -- UNION
 /* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
@@ -70,6 +130,42 @@ HINT: There are a possibly a few ways to do this query, but if you're struggling
 3) Query the second temp table twice, once for the best day, once for the worst day, 
 with a UNION binding them. */
 
+WITH sales_by_date AS (
+    SELECT
+        market_date,
+        SUM(quantity * cost_to_customer_per_qty) as total_sales
+    FROM
+        customer_purchases
+    GROUP BY
+        market_date
+),
+ranked_sales AS (
+    SELECT 
+        market_date,
+        total_sales,
+        ROW_NUMBER() OVER (ORDER BY total_sales DESC) as sales_rank_desc,
+        ROW_NUMBER() OVER (ORDER BY total_sales ASC) as sales_rank_asc
+    FROM
+        sales_by_date
+)
 
+-- 'Highest Total Sales'
+SELECT
+    market_date,
+    total_sales,
+    'Highest Total Sales' as sales_type
+FROM
+    ranked_sales
+WHERE
+    sales_rank_desc = 1
 
-
+-- 'Lowest Total Sales'
+UNION
+SELECT
+    market_date,
+    total_sales,
+    'Lowest Total Sales' as sales_type
+FROM
+    ranked_sales
+WHERE
+    sales_rank_asc = 1;
